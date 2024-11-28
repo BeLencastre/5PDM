3619cd7973970c77fd846acf827eac3bb6baf572
Bernardo <blencastre@sinaf.com.br>
2024-11-28 18:12:30 -0300

    projeto av2

---
+++ livrook/src/app/componentes/cabecalho/cabecalho.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cabecalho',
+  templateUrl: './cabecalho.component.html',
+  styleUrls: ['./cabecalho.component.css']
+})
+export class CabecalhoComponent {
+
+  constructor(private router: Router) {}
+
+}
